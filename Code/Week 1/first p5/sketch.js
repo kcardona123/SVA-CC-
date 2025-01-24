9efbbfd06f5e3ea1c0f038cf2a6d331f3fcf675b
kevin cardona cervantes <kcardonacervantes@sva.edu>
2025-01-23 21:25:19 -0500

    last edits

--- Code/Week 1/first p5/sketch.js
+++ Code/Week 1/first p5/sketch.js
@@ -1,6 +1,6 @@
 let btn;
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
     rectMode(CENTER);
 
     btn = select('#my-button');
@@ -15,4 +15,10 @@ function setup() {
   function draw() {
     background(220);
     rect(100,100,100);
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+  }
+  function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
   }
