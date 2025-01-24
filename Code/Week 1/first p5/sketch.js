bdcf1b0b4d4bd56f34601eb182727cb6d6f3de60
kevin cardona cervantes <kcardonacervantes@sva.edu>
2025-01-23 20:12:17 -0500

    button event

--- Code/Week 1/first p5/sketch.js
+++ Code/Week 1/first p5/sketch.js
@@ -1,8 +1,17 @@
+let btn;
 function setup() {
     createCanvas(400, 400);
     rectMode(CENTER);
+
+    btn = select('#my-button');
+    btn.mouseClicked(onBtnClicked);
+
   }
-  
+
+  function onBtnClicked(){
+    fill(random(255));
+  }
+
   function draw() {
     background(220);
     rect(100,100,100);
