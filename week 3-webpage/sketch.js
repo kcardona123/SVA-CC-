25eb26e9c1cd71bc2ac543c0c81235dc7fa37117
kevin cardona cervantes <kcardonacervantes@sva.edu>
2025-01-30 20:09:50 -0500

    for loop

--- week 3-webpage/sketch.js
+++ week 3-webpage/sketch.js
@@ -1,5 +1,27 @@
+let links =[];
+
 function setup() {
-  createCanvas(500, 500);
-  background(190);
-  ellipse(100, 100, 100);
+    for (let i = 0; i<25; i++ ) {
+      const link = createA("#", "Yahoo!!");
+      link.position (random(500), random(500));
+      link.mouseClicked(onMouseClick);
+      link.mouseOver(onMouseOver);
+      link.mouseOut(onMouseOut);
+      links.push(link);
 }
+}
+
+ function onMouseClick() {
+  this.style("background-color", "green");
+  this.style("background-color", "white");
+ }
+
+ function onMouseOver() {
+  this.style("background-color", "pink");
+  this.style("color", "black");
+ }
+
+ function onMouseOut() {
+  this.style("background-color", "red");
+  this.style("color", "white");
+ }
